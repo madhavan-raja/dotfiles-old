1dfe1c9eaec34eec6c1077af8be0a90e5360f734
Madhavan Raja <madhavanraja99@gmail.com>
2022-09-25 06:01:57 -0700

    Updated polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+# Add this script to your WM startup file.
+
+DIR="$HOME/.config/polybar"
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload main &
-done
-
-echo "Bars launched..."
+# Launch the bar
+polybar -q main -c "$DIR/config.ini" &
